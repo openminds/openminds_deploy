ec0280e55b8eb0714e5cd0827f15dd838d9886ca
Jan De Poorter <jan@defv.be>
2010-10-04 19:08:32 +0200

    Add lighttpd recipe

---
+++ lib/openminds_deploy/lighttpd.rb
@@ -0,0 +1,40 @@
+configuration = Capistrano::Configuration.respond_to?(:instance) ? Capistrano::Configuration.instance(:must_exist) : Capistrano.configuration(:must_exist)
+
+configuration.load do
+  # Lighttpd stuff
+  namespace :lighttpd do
+    desc "Restart the web server"
+    task :restart, :roles => :app do
+      run "lighty restart"
+    end
+
+    desc "Stop the web server"
+    task :stop, :roles => :app do
+      run "lighty stop"
+    end
+
+    desc "Start the web server"
+    task :start, :roles => :app do
+      run "lighty start"
+    end
+  end
+
+  # Standard deploy actions overwritten
+  namespace :deploy do
+    desc "Restart your application"
+    task :restart do
+      lighttpd::restart
+    end
+
+    desc "Start your application"
+    task :start do
+      lighttpd::start
+    end
+
+    desc "Stop your application"
+    task :stop do
+      lighttpd::stop
+    end
+  end
+end
+
