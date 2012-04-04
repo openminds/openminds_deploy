ffeeabfe519d610fb1e51ee35c6933ab728eddd5
Jeroen Jacobs <git@jeroenj.be>
2012-04-04 10:24:01 +0200

    Moves migrate task to defaults since it works for older rails versions too

--- lib/openminds_deploy/defaults.rb
+++ lib/openminds_deploy/defaults.rb
@@ -30,6 +30,19 @@ configuration.load do
 
   after :deploy, 'deploy:cleanup'
 
+  desc 'Prompts if new migrations are available and runs them if you want to'
+  namespace :deploy do
+    task :needs_migrations, :roles => :db, :only => {:primary => true} do
+      old_rev = capture("cd #{previous_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+      new_rev = capture("cd #{latest_release} && git log --pretty=format:'%H' -n 1 | cat").strip
+      migrations_changed = capture("cd #{latest_release} && git diff #{old_rev} #{new_rev} --name-only | cat").include?('db/migrate')
+      if migrations_changed && Capistrano::CLI.ui.ask("New migrations pending. Enter 'yes' to run db:migrate") == 'yes'
+        migrate
+      end
+    end
+  end
+  after 'deploy:update_code', 'deploy:needs_migrations'
+
   desc 'Tail application log'
   task :tail_log, :roles => :app do
     sudo "tail -f #{shared_path}/log/#{rails_env}.log"
